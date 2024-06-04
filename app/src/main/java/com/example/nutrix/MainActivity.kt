e8d41962eaf90fe3db7bb22a0d622b933d9b4e17
Gilang <gilangnurrizki173@gmail.com>
2024-06-04 22:12:06 +0700

    styling main

--- app/src/main/java/com/example/nutrix/MainActivity.kt
+++ app/src/main/java/com/example/nutrix/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.nutrix
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val progressKarbo = findViewById<ProgressBar>(R.id.progress_karbohidrat)
+        val progressProtein = findViewById<ProgressBar>(R.id.progress_protein)
+        val progressLemak = findViewById<ProgressBar>(R.id.progress_lemak)
+        val progressVitamin = findViewById<ProgressBar>(R.id.progress_vitamin)
+        val progressMineral = findViewById<ProgressBar>(R.id.progress_mineral)
+
+        progressKarbo.progress = 50
+        progressProtein.progress = 70
+        progressLemak.progress = 30
+        progressVitamin.progress = 90
+        progressMineral.progress = 45
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.profile_page -> {
+                startActivity(Intent(this, UserActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/example/nutrix/SplashActivity.kt
+++ app/src/main/java/com/example/nutrix/SplashActivity.kt
@@ -1,20 +1,26 @@
 package com.example.nutrix
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val decorView = window.decorView
+        // Hide the status bar.
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // Hide ActionBar
+        supportActionBar?.hide()
+        // Timer
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }

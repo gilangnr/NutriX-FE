345c8a5dbdca8582fa85d46868d35b1efd05ebe2
Gilang <gilangnurrizki173@gmail.com>
2024-06-11 21:47:12 +0700

    user activity

--- app/src/main/java/com/example/nutrix/UserActivity.kt
+++ app/src/main/java/com/example/nutrix/UserActivity.kt
@@ -1,20 +1,19 @@
 package com.example.nutrix
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class UserActivity : AppCompatActivity() {
+
+    private lateinit var back:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_user)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        supportActionBar?.hide()
+
+        back = findViewById(R.id.back)
+
+        back.setOnClickListener { finish() }
     }
 }

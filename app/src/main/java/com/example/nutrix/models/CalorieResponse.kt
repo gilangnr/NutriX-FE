7c63fe184249600b93f0b2ba44554cff34695fd1
gerry <gerrydesrian488@gmail.com>
2024-06-24 09:41:51 +0700

    update route

--- app/src/main/java/com/example/nutrix/models/CalorieResponse.kt
+++ app/src/main/java/com/example/nutrix/models/CalorieResponse.kt
@@ -1,5 +1,26 @@
 package com.example.nutrix.models
 
 data class CalorieResponse(
-    val foodInfo: FoodInfo
+    val status: Int,
+    val message: String,
+    val foodInfo: FoodInfo,
+    val totalNutrition: TotalNutrition
 )
+
+data class FoodInfo(
+    val foodName: String,
+    val calorie: Float,
+    val carbohydrate: Float,
+    val fat: Float,
+    val protein: Float,
+    val sugar: Float
+)
+
+data class TotalNutrition(
+    val totalCalories: Float,
+    val totalCarbohydrate: Float,
+    val totalProtein: Float,
+    val totalFat: Float,
+    val totalSugar: Float
+)
+
